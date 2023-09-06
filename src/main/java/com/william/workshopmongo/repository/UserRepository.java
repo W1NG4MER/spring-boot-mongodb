f654588ec7e41808a624c95e66a68e72696c58e8
W1NG4MER <wingamer.oliveiradossantos26@gmail.com>
2023-09-06 11:01:11 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/william/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.william.workshopmongo.repository;
+
+import com.william.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
---
+++ src/main/java/com/william/workshopmongo/services/UserService.java
@@ -0,0 +1,19 @@
+package com.william.workshopmongo.services;
+
+import com.william.workshopmongo.domain.User;
+import com.william.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll() {
+        return repo.findAll();
+    }
+}
