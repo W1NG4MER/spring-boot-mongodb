2d6712ac6fd16ff2f6c9f567659510f7b252f8c2
W1NG4MER <wingamer.oliveiradossantos26@gmail.com>
2023-09-12 10:54:33 -0300

    Atualização de usuário com PUT

--- src/main/java/com/william/workshopmongo/resources/UserResource.java
+++ src/main/java/com/william/workshopmongo/resources/UserResource.java
@@ -27,7 +27,7 @@ public class UserResource {
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public ResponseEntity<UserDTO> findById (@PathVariable String id) {
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
         User obj = service.findById(id);
         return ResponseEntity.ok().body(new UserDTO(obj));
     }
@@ -41,8 +41,16 @@ public class UserResource {
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<Void> delete (@PathVariable String id) {
+    public ResponseEntity<Void> delete(@PathVariable String id) {
        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}",method = RequestMethod.PUT)
+    public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+        User obj = service.fromDTO(objDto);
+        obj.setId(id);
+        obj = service.update(obj);
         return ResponseEntity.noContent().build();
     }
 }
