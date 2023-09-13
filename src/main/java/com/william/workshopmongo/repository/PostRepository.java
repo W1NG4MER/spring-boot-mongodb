1dca9e7077d2a78054da7f7a8c6ea3c2d748502d
W1NG4MER <wingamer.oliveiradossantos26@gmail.com>
2023-09-13 09:32:55 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/william/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.william.workshopmongo.repository;
+
+import com.william.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
